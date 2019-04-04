19311c635619cb11dfaa04cb47a7772a4e5fd344
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-04-04 17:09:15 -0400

    MVP day 2 done

--- frontend/src/components/Users.js
+++ frontend/src/components/Users.js
@@ -12,7 +12,7 @@ class Users extends Component{
                <h1>Users</h1>
                <ul>
                    {this.state.users.map(user => (
-                       <li>{user.username}</li>
+                       <li key = {user.id}>{user.username}</li>
                    ))}
                </ul>
             </>
@@ -22,8 +22,11 @@ class Users extends Component{
     componentDidMount(){
         axios.get('/users').then(res => {
             console.log(res.data)
+            this.setState({
+                users: res.data
+            })
         }).catch(err => {
-            console.log(err)
+            console.log(err.message)
         })
     }
 }
